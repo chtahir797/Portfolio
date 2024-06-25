cd759b23d0a9de3a21ddcdc74def98b4fe513d7d
chtahir797 <chtahir797@gmail.com>
2024-06-25 23:01:37 +0500

    adding frontpage

---
+++ assets/js/particlesConfig.js
@@ -0,0 +1,88 @@
+
+tsParticles.load("tsparticles", {
+  particles: {
+      number: {
+          value: 80,
+          density: {
+              enable: true,
+              value_area: 800
+          }
+      },
+      color: {
+          value: ["#1abc9c", "#3498db", "#9b59b6", "#e74c3c", "#f39c12", "#2ecc71"] // Array of colorful values
+      },
+      shape: {
+          type: "circle"
+      },
+      links: {
+          enable: true,
+          color: "#000000",
+          distance: 150,
+          opacity: 0.4,
+          width: 1
+      },
+      move: {
+          enable: true,
+          speed: 2, // Adjusted speed
+          direction: "none",
+          random: false,
+          straight: false,
+          out_mode: "out",
+          bounce: false,
+          attract: {
+              enable: false,
+              rotateX: 600,
+              rotateY: 1200
+          }
+      },
+      size: {
+          value: 4,
+          random: true,
+          anim: {
+              enable: false,
+              speed: 40,
+              size_min: 0.1,
+              sync: false
+          }
+      }
+  },
+  interactivity: {
+    detectsOn: "window",
+    events: {
+        onHover: {
+            enable: true,
+            mode: "repulse"
+        },
+        onClick: {
+            enable: true,
+            mode: "push"
+        }
+    },
+      modes: {
+          grab: {
+              distance: 400,
+              line_linked: {
+                  opacity: 1
+              }
+          },
+          bubble: {
+              distance: 400,
+              size: 40,
+              duration: 2,
+              opacity: 8,
+              speed: 3
+          },
+          repulse: {
+              distance: 100,
+              duration: 0.4
+          },
+          push: {
+              particles_nb: 4
+          },
+          remove: {
+              particles_nb: 2
+          }
+      }
+  },
+  retina_detect: true
+});
---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+//media query code
+let width = window.matchMedia("(width < 760px)")
+const menu = document.querySelector('.menu-list')
+const hamburger = document.querySelector('#hamburger')
+const cross = document.querySelector('.cross')
+width.addEventListener('change', ()=>{
+    if(width.matches){
+        menu.style.display = 'none'
+        hamburger.style.display = 'block'
+    } else{
+        menu.style.display = 'block'
+        hamburger.style.display = 'none'
+    }
+})
+
+hamburger.addEventListener('click', () => {
+    // menu.style.display = menu.style.display === 'none' ? 'block' : 'none';
+    if (menu.style.display === 'none' || menu.style.display === '') {
+        menu.style.display = 'block';
+    } else {
+        menu.style.display = 'none';
+    }
+});
+
+cross.addEventListener('click', ()=>{
+    menu.style.display = 'none';
+})
